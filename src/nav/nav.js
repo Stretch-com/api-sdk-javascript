ffdd7cbd4e747a1fd0ad33b72a9b0800fa957a4f
Haitham <haitham@stretch.com>
2023-07-05 17:42:34 +0400

    update nav sdk

--- src/nav/nav.js
+++ src/nav/nav.js
@@ -1,31 +1,44 @@
 class Nav {
-    constructor(client) {
-        this._client = client;
-    }
+  constructor(client) {
+    this._client = client;
+  }
 
-    async getLocations() {
-        if (await this.checkAuth()) {
-            return await this._client.get(`/nav/locations`);
-        }
+  async getLocations() {
+    if (await this.checkAuth()) {
+      return await this._client.get(`/nav/locations`);
     }
+  }
 
-    async postLocation(location) {
-        if (await this.checkAuth()) {
-            return await this._client.post(`/nav/location`, location);
-        }
+  async postLocation(location) {
+    if (await this.checkAuth()) {
+      return await this._client.post(`/nav/location`, location);
     }
+  }
 
-    async putLocation(locationId, location) {
-        if (await this.checkAuth()) {
-            return await this._client.put(`/nav/location/${locationId}`, location);
-        }
+  async putLocation(locationId, location) {
+    if (await this.checkAuth()) {
+      return await this._client.put(`/nav/location/${locationId}`, location);
     }
+  }
 
-    async deleteLocation(locationId, location) {
-        if (await this.checkAuth()) {
-            return await this._client.delete(`/nav/location/${locationId}`, location);
-        }
+  async deleteLocation(locationId, location) {
+    if (await this.checkAuth()) {
+      return await this._client.delete(`/nav/location/${locationId}`, location);
     }
+  }
+
+  // Geolocation & geocoding
+  async geosearch(query) {
+    if (await this.checkAuth()) {
+      return await this._client.delete(`/nav/geosearch`, query);
+    }
+  }
+
+  async geocoding(lng, lat) {
+    if (await this.checkAuth()) {
+      return await this._client.delete(`/nav/geocoding/${lng},${lat}`);
+    }
+  }
 }
 
 export default Nav;
