61cab893da04c8d0e9594ea617467f4ea0da156a
Haitham <haitham@stretch.com>
2023-07-10 15:09:44 +0400

    add drop user sdk

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,37 +1,38 @@
-export class Auth{
+export class Auth {
+  constructor(client) {
+    //
+    this._client = client;
+  }
 
-    constructor(client) { //
-        this._client = client;
+  async getUser() {
+    if (await this._client.checkAuth()) {
+      return await this._client.get(`/auth/user`);
     }
+  }
 
-    async getUser(){
-        if (await this._client.checkAuth()){
-            return await this._client.get(`/auth/user`)
-        }
+  async putUser(payload = {}) {
+    if (await this._client.checkAuth()) {
+      return await this._client.put(`/auth/user`, payload);
     }
+  }
 
-    async putUser(payload = {}){
-        if (await this._client.checkAuth()){
-            return await this._client.put(`/auth/user`, payload)
-        }
+  async dropUser() {
+    if (await this._client.checkAuth()) {
+      return await this._client.delete(`/auth/user`);
     }
+  }
 
-    async putPasswordReset(payload = {}){
-        if (await this._client.checkAuth()){
-            return await this._client.put(`/auth/password-reset`, payload)
-        }
+  async putPasswordReset(payload = {}) {
+    if (await this._client.checkAuth()) {
+      return await this._client.put(`/auth/password-reset`, payload);
     }
+  }
 
-    async postPasswordReset(payload = {}){
-        if (await this._client.checkAuth()){
-            return await this._client.post(`/auth/password-reset`, payload)
-        }
+  async postPasswordReset(payload = {}) {
+    if (await this._client.checkAuth()) {
+      return await this._client.post(`/auth/password-reset`, payload);
     }
-
-
-
-
-
+  }
 }
 
 export default Auth;
