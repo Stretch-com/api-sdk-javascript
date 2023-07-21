27ba79f23a12cb64c06dd3e11d37293d3fce1491
haitham <haitham@stretch.com>
2023-07-21 16:11:18 +0400

    admin session endpoints

--- src/admin/admin.js
+++ src/admin/admin.js
@@ -1,46 +1,65 @@
-export class Admin{
+export class Admin {
+  constructor(client) {
+    this._client = client;
+  }
 
-    constructor(client) { //
-        this._client = client;
+  // Coaches endpoints
+  async getCoaches(query = {}) {
+    if (await this._client.checkAuth()) {
+      return await this._client.get(`/admin/coaches`, query);
     }
+  }
 
-    async getCoaches(query={}){
-        if (await this._client.checkAuth()){
-            return await this._client.get(`/admin/coaches`, query)
-        }
+  async getCoachesCount(query = {}) {
+    if (await this._client.checkAuth()) {
+      return await this._client.get(`/admin/coaches/count`, query);
     }
+  }
 
-    async getCoachesCount(query={}){
-        if (await this._client.checkAuth()){
-            return await this._client.get(`/admin/coaches/count`, query)
-        }
+  async postCoach(payload = {}) {
+    if (await this._client.checkAuth()) {
+      return await this._client.post(`/admin/coach`, payload);
     }
+  }
 
-    async postCoach(payload={}){
-        if (await this._client.checkAuth()){
-            return await this._client.post(`/admin/coach`, payload)
-        }
+  // Clients endpoints
+  async getClients(query = {}) {
+    if (await this._client.checkAuth()) {
+      return await this._client.get(`/admin/clients`, query);
     }
+  }
 
-    async getClients(query={}){
-        if (await this._client.checkAuth()){
-            return await this._client.get(`/admin/clients`, query)
-        }
+  async getClientsCount(query = {}) {
+    if (await this._client.checkAuth()) {
+      return await this._client.get(`/admin/clients/count`, query);
     }
+  }
 
-    async getClientsCount(query={}){
-        if (await this._client.checkAuth()){
-            return await this._client.get(`/admin/clients/count`, query)
-        }
+  // Feedback endpoints
+  async postSupportFeedback(form) {
+    if (await this._client.checkAuth()) {
+      return await this._client.postForm(`/admin/support/feedback`, form);
     }
+  }
 
-
-
-    async postSupportFeedback(form){
-        if (await this._client.checkAuth()){
-            return await this._client.postForm(`/admin/support/feedback`, form)
-        }
+  // Sessions endpoints
+  async getSessions(query = {}) {
+    if (await this._client.checkAuth()) {
+      return await this._client.get("/admin/sessions", query);
     }
+  }
+
+  async getSessionCount(query = {}) {
+    if (await this._client.checkAuth()) {
+      return await this._client.get("/admin/session/count", query);
+    }
+  }
+
+  async getSessionStates() {
+    if (await this._client.checkAuth()) {
+      return await this._client.get("/admin/sessionstates");
+    }
+  }
 }
 
 export default Admin;
