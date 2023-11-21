71e2e0bafa23730bd2d7f0ef1cfa5aefe07c8d17
haitham <haitham@stretch.com>
2023-11-21 14:05:40 +0400

    get coverage api

--- src/nav/nav.js
+++ src/nav/nav.js
@@ -4,46 +4,44 @@ class Nav {
   }
 
   async getLocation() {
-    if (await this._client.checkAuth()) {
+    if (await this._client.checkAuth())
       return await this._client.get(`/nav/location`);
-    }
   }
 
   async getLocations() {
-    if (await this._client.checkAuth()) {
+    if (await this._client.checkAuth())
       return await this._client.get(`/nav/locations`);
-    }
   }
 
   async postLocation(location) {
-    if (await this._client.checkAuth()) {
+    if (await this._client.checkAuth())
       return await this._client.post(`/nav/location`, location);
-    }
   }
 
   async putLocation(locationId, location) {
-    if (await this._client.checkAuth()) {
+    if (await this._client.checkAuth())
       return await this._client.put(`/nav/location/${locationId}`, location);
-    }
   }
 
   async deleteLocation(locationId, location) {
-    if (await this._client.checkAuth()) {
+    if (await this._client.checkAuth())
       return await this._client.delete(`/nav/location/${locationId}`, location);
-    }
   }
 
   // Geolocation & geocoding
   async geosearch(query) {
-    if (await this._client.checkAuth()) {
+    if (await this._client.checkAuth())
       return await this._client.get(`/nav/geosearch`, query);
-    }
   }
 
   async geocoding(lng, lat) {
-    if (await this._client.checkAuth()) {
+    if (await this._client.checkAuth())
       return await this._client.get(`/nav/geocoding/${lng},${lat}`);
-    }
+  }
+
+  async getCoverage(query = {}) {
+    if (await this._client.checkAuth())
+      return await this._client.get(`/nav/coverage`, query);
   }
 }
 
