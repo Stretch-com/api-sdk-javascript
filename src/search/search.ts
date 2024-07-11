6ff4a4364d0a0fd72da7fd066c0d816901977393
Haitham <haitham@stretch.com>
2024-07-11 14:43:53 +0400

    Added marketing groups API

--- src/search/search.ts
+++ src/search/search.ts
@@ -29,13 +29,43 @@ export class Search {
   }
 
   async getFilter(
-    query: paths["/api/v1/search/filter"]["get"]["parameters"]["query"]
+    query: paths["/api/v1/search/filter"]["get"]["parameters"]["query"] = {}
   ): Promise<
     | paths["/api/v1/search/filter"]["get"]["responses"]["200"]["content"]["application/json"]
     | undefined
   > {
     if (await this.auth.checkAuth())
       return await this.auth.get(`/search/filter`, query);
+  }
+
+  async postBusinessSearch(
+    payload: paths["/api/v1/search/business"]["post"]["requestBody"]["content"]["application/json"]
+  ): Promise<
+    | paths["/api/v1/search/business"]["post"]["responses"]["200"]["content"]["application/json"]
+    | undefined
+  > {
+    if (await this.auth.checkAuth())
+      return await this.auth.post(`/search/business`, payload);
+  }
+
+  async postBusinessCount(
+    payload: paths["/api/v1/search/business/count"]["post"]["requestBody"]["content"]["application/json"]
+  ): Promise<
+    | paths["/api/v1/search/business/count"]["post"]["responses"]["200"]["content"]["application/json"]
+    | undefined
+  > {
+    if (await this.auth.checkAuth())
+      return await this.auth.post(`/search/business/count`, payload);
+  }
+
+  async getBusinessFilter(
+    query: paths["/api/v1/search/business/filter"]["get"]["parameters"]["query"] = {}
+  ): Promise<
+    | paths["/api/v1/search/business/filter"]["get"]["responses"]["200"]["content"]["application/json"]
+    | undefined
+  > {
+    if (await this.auth.checkAuth())
+      return await this.auth.get(`/search/business/filter`, query);
   }
 }
 
